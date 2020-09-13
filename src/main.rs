2389b7e7772b93f54ce16f6bb373ca3dbbd061d8
Cole Mickens <cole.mickens@gmail.com>
2020-09-13 21:36:24 +0000

    added 'add' and 'print_sum' functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,17 @@
+fn add(a: u32, b: u32) -> u32 {
+    return a + b;
+}
+
+fn print_sum(a: u32, b: u32) {
+    println!("print_sum says {}", a + b);
+}
+
 fn main() {
-    println!(
-        "The answer to the ultimate question is {}",
-        rust_gh_example::answer()
-    );
+    let sum = add(1, 2);
+    println!("the sum is {}", sum);
+
+    print_sum(5, 6);
+
+    let zzzzz = rust_gh_example::answer();
+    println!("Answer is {}", zzzzz);
 }
