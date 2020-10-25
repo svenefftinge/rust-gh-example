99ed3af3a19ac5f931dcf9967364df4c2955254e
prolham <budpetrey@gmail.com>
2020-10-25 00:59:52 +0000

    Added iterative factorial function

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,14 @@ pub fn answer() -> u32 {
     42
 }
 
+pub fn factorial(n: u32) -> u32 {
+    let mut fact = 1;
+    
+    for num in 1..(n+1) {
+       fact *= num; 
+    } 
+    return fact;
+}
 #[cfg(test)]
 mod tests {
     use super::*;
